88e266f666d0f8287fc2bafee4aa92609c4196a0
Daniel Clelland <daniel.clelland@gmail.com>
2019-10-21 22:46:30 +1300

    Trying out Pipe

--- Urbit/AppDelegate.swift
+++ Urbit/AppDelegate.swift
@@ -11,7 +11,38 @@ import AppKit
 @NSApplicationMain class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
+        let outputPipe = Pipe()
+        let errorPipe = Pipe()
+        
         let process = Process.restartFakeZod()
+        process.standardOutput = outputPipe
+        process.standardError = errorPipe
+        outputPipe.fileHandleForReading.waitForDataInBackgroundAndNotify()
+        errorPipe.fileHandleForReading.waitForDataInBackgroundAndNotify()
+//        outputPipe.fileHandleForReading.readabilityHandler = { file in
+//            let string = String(data: file.availableData, encoding: .utf8)!
+//            print("output:", string)
+//        }
+
+        NotificationCenter.default.addObserver(forName: .NSFileHandleDataAvailable, object: outputPipe.fileHandleForReading, queue: nil) { notification in
+            let string = String(data: outputPipe.fileHandleForReading.availableData, encoding: .utf8)!
+            print("OUTPUT:")
+            print(string)
+        }
+
+        NotificationCenter.default.addObserver(forName: .NSFileHandleDataAvailable, object: errorPipe.fileHandleForReading, queue: nil) { notification in
+            let string = String(data: errorPipe.fileHandleForReading.availableData, encoding: .utf8)!
+            print("ERROR:")
+            print(string)
+        }
+        
+//        outputPipe.fileHandleForReading.waitForDataInBackgroundAndNotify()
+        
+        process.terminationHandler = { process in
+            print("process terminated")
+//            outputPipe.fileHandleForReading.readabilityHandler = nil
+        }
+        
         process.launch()
         process.waitUntilExit()
     }
